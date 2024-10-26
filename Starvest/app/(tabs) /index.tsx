d4d81e9c3b3c4b06da1e7777af9edff6acdf081b
sabaflz <sabaa.feilizadeh@gmail.com>
2024-10-25 18:57:27 -0700

    home page and the simple navbar (using expo)

---
+++ Starvest/app/(tabs) /index.tsx
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+      <Link href="/account" style={styles.button}>
+        Go to your Account page
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
