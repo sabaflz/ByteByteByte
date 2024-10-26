b87a53f6f7cbdae835725cfd7efc727524d0a6f1
Anna Kuskova <akuskova@MacBook-Pro.local>
2024-10-26 09:26:21 -0700

    weather api

--- Starvest/app/_layout.tsx
+++ Starvest/app/_layout.tsx
@@ -1,10 +1,46 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
+import { useColorScheme } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  );
+export default function TabLayout() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: '#ffd33d',
+                headerStyle: { backgroundColor: '#376443' },
+                headerShadowVisible: false,
+                headerTintColor: '#fff',
+                tabBarStyle: { backgroundColor: '#376443' },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons 
+                            name={focused ? 'home-sharp' : 'home-outline'} 
+                            color={color} 
+                            size={24} 
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="account"
+                options={{
+                    title: 'Account',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons 
+                            name={focused ? 'information-circle' : 'information-circle-outline'} 
+                            color={color} 
+                            size={24} 
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
